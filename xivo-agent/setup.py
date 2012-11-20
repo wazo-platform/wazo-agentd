d2e57c0fd63ea0108335f6ef07b78c912f8ae01a
Etienne Lessard <elessard@avencall.com>
2012-11-20 08:39:43 -0500

    add setup.py

---
+++ xivo-agent/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='xivo-agent',
+    version='0.1',
+    description='XiVO agent server and client',
+    author='Avencall',
+    author_email='dev@avencall.com',
+    url='http://git.xivo.fr/',
+    license='GPLv3',
+    packages=['xivo_agent',
+              'xivo_agent.ami',
+              'xivo_agent.ami.actions',
+              'xivo_agent.ami.actions.common',
+              'xivo_agent.bin',
+              'xivo_agent.ctl'],
+    scripts=['bin/xivo-agentd',
+             'bin/xivo-agentctl'],
+)
