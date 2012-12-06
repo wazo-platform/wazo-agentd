30d45e2d2f7968744324482352698ce4e7275c10
Gregory Eric Sanderson <gsanderson@avencall.com>
2012-12-06 11:08:01 -0500

    refactor xivo-agentctl, add -c to send agent commands through shell

--- xivo-agent/xivo_agent/bin/agentctl.py
+++ xivo-agent/xivo_agent/bin/agentctl.py
@@ -15,20 +15,60 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+import argparse
 import readline
 import time
 import traceback
 from xivo_agent.ctl.client import AgentClient
 from xivo_agent.exception import AgentError
 
+from contextlib import contextmanager
 
-def main():
+@contextmanager
+def _agent_client():
     agent_client = AgentClient()
+    agent_client.connect('localhost')
     try:
-        agent_client.connect('localhost')
-        _loop(agent_client)
+        yield agent_client
     finally:
         agent_client.close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--command', help='Run command')
+
+    opts = parser.parse_args()
+
+    with _agent_client() as agent_client:
+        if opts.command:
+            _execute_command(opts.command, agent_client)
+        else:
+            _loop(agent_client)
+
+
+def _execute_command(line, agent_client):
+    tokens = line.split()
+    cmd_name, args = tokens[0], tokens[1:]
+    start_time = time.time()
+    if cmd_name == 'login':
+        agent_number = args[0]
+        extension = args[1]
+        context = args[2]
+        agent_client.login_agent_by_number(agent_number, extension, context)
+    elif cmd_name == 'logoff':
+        agent_number = args[0]
+        agent_client.logoff_agent_by_number(agent_number)
+    elif cmd_name == 'status':
+        agent_number = args[0]
+        print agent_client.get_agent_status_by_number(agent_number)
+    elif cmd_name == 'statuses':
+        print agent_client.get_agent_statuses()
+    elif cmd_name == 'ping':
+        print agent_client.ping()
+    else:
+        print 'unknown command:', cmd_name
+    print 'request took %s ms' % ((time.time() - start_time) * 1000)
 
 
 def _loop(agent_client):
@@ -39,29 +79,8 @@ def _loop(agent_client):
                 if not line:
                     continue
 
-                tokens = line.split()
-                cmd_name, args = tokens[0], tokens[1:]
-                start_time = time.time()
-                if cmd_name == 'login':
-                    agent_number = args[0]
-                    extension = args[1]
-                    context = args[2]
-                    agent_client.login_agent_by_number(agent_number, extension, context)
-                elif cmd_name == 'logoff':
-                    agent_number = args[0]
-                    agent_client.logoff_agent_by_number(agent_number)
-                elif cmd_name == 'status':
-                    agent_number = args[0]
-                    print agent_client.get_agent_status_by_number(agent_number)
-                elif cmd_name == 'statuses':
-                    print agent_client.get_agent_statuses()
-                elif cmd_name == 'ping':
-                    print agent_client.ping()
-                else:
-                    print 'unknown command:', cmd_name
-                    continue
+                _execute_command(line, agent_client)
 
-                print 'request took %s ms' % ((time.time() - start_time) * 1000)
             except KeyboardInterrupt:
                 print
             except EOFError:
