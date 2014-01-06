152374a68104873027bc1c685e67c033a3578606
Sebastien Duthil <sduthil@avencall.com>
2014-01-06 16:59:12 -0500

    factorize duplicate test code

--- xivo-agent/xivo_agent/tests/test_queuelog.py
+++ xivo-agent/xivo_agent/tests/test_queuelog.py
@@ -17,69 +17,81 @@
 
 import datetime
 import unittest
+from functools import wraps
 from mock import ANY, Mock, patch, sentinel
 from xivo_agent.queuelog import QueueLogManager
+
+mock_date = datetime.datetime(2011, 11, 12, 13, 14, 15, 1617)
+mock_date_str = '2011-11-12 13:14:15.001617'
+
+
+class patch_datetime_now(object):
+    def __init__(self, *mock_args, **mock_kwargs):
+        self.mock_args = mock_args
+        self.mock_kwargs = mock_kwargs
+
+    def __call__(self, wrapped):
+        @wraps(wrapped)
+        def wrapper(*args, **kwargs):
+            with patch('datetime.datetime') as datetime_mock:
+                datetime_mock.now = Mock(*self.mock_args, **self.mock_kwargs)
+                wrapped(*args, **kwargs)
+        return wrapper
 
 
 class TestQueuelog(unittest.TestCase):
 
     def test_format_time(self):
-        dt = datetime.datetime(2011, 11, 12, 13, 14, 15, 1617)
-        expected = '2011-11-12 13:14:15.001617'
+        dt = mock_date
+        expected = mock_date_str
 
         value = QueueLogManager.format_time(dt)
 
         self.assertEqual(expected, value)
 
+    @patch_datetime_now(return_value=mock_date)
     def test_format_time_now(self):
-        dt_now = datetime.datetime(2011, 11, 12, 13, 14, 15, 1617)
-        str_now = '2011-11-12 13:14:15.001617'
-        with patch('datetime.datetime') as datetime_mock:
-            datetime_mock.now.return_value = dt_now
+        expected = mock_date_str
 
-            value = QueueLogManager.format_time_now()
+        value = QueueLogManager.format_time_now()
 
-            self.assertEqual(str_now, value)
+        self.assertEqual(expected, value)
 
+    @patch_datetime_now(return_value=mock_date)
     def test_on_agent_logged_in(self):
-        dt_now = datetime.datetime(2011, 11, 12, 13, 14, 15, 1617)
-        str_now = '2011-11-12 13:14:15.001617'
-        with patch('datetime.datetime') as datetime_mock:
-            datetime_mock.now.return_value = dt_now
-            queue_log_dao = Mock()
-            queue_log_mgr = QueueLogManager(queue_log_dao)
+        str_now = mock_date_str
+        queue_log_dao = Mock()
+        queue_log_mgr = QueueLogManager(queue_log_dao)
 
-            queue_log_mgr.on_agent_logged_in('1', '1001', 'default')
+        queue_log_mgr.on_agent_logged_in('1', '1001', 'default')
 
-            queue_log_dao.insert_entry.assert_called_once_with(
-                str_now,
-                'NONE',
-                'NONE',
-                'Agent/1',
-                'AGENTCALLBACKLOGIN',
-                '1001@default',
-            )
+        queue_log_dao.insert_entry.assert_called_once_with(
+            str_now,
+            'NONE',
+            'NONE',
+            'Agent/1',
+            'AGENTCALLBACKLOGIN',
+            '1001@default',
+        )
 
+    @patch_datetime_now(return_value=mock_date)
     def test_on_agent_logged_off(self):
-        dt_now = datetime.datetime(2011, 11, 12, 13, 14, 15, 1617)
-        str_now = '2011-11-12 13:14:15.001617'
-        with patch('datetime.datetime') as datetime_mock:
-            datetime_mock.now.return_value = dt_now
-            queue_log_dao = Mock()
-            queue_log_mgr = QueueLogManager(queue_log_dao)
+        str_now = mock_date_str
+        queue_log_dao = Mock()
+        queue_log_mgr = QueueLogManager(queue_log_dao)
 
-            queue_log_mgr.on_agent_logged_off('1', '1001', 'default', 123)
+        queue_log_mgr.on_agent_logged_off('1', '1001', 'default', 123)
 
-            queue_log_dao.insert_entry.assert_called_once_with(
-                str_now,
-                'NONE',
-                'NONE',
-                'Agent/1',
-                'AGENTCALLBACKLOGOFF',
-                '1001@default',
-                '123',
-                'CommandLogoff',
-            )
+        queue_log_dao.insert_entry.assert_called_once_with(
+            str_now,
+            'NONE',
+            'NONE',
+            'Agent/1',
+            'AGENTCALLBACKLOGOFF',
+            '1001@default',
+            '123',
+            'CommandLogoff',
+        )
 
     def test_on_agent_logged_off_written_logged_time_should_be_an_integer_when_given_logged_time_is_not_integer(self):
         queue_log_dao = Mock()
