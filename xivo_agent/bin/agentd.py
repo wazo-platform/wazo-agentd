da1e5ee228a3511e10378062e6600fd8d3bc5130
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2014-12-09 13:32:52 -0500

    add the mechanism to read and merge config files
    I used the same mechanism as xivo-ctid for the config files
    the main config file /etc/xivo-agent/config.yml is maintained by the packager and should
    not be modified by the administrator.
    
    Extra configuration files stored in /etc/xivo-agentd/conf.d override content from the
    main configuration file and will be in alphabetical order or priority

--- xivo_agent/bin/agentd.py
+++ xivo_agent/bin/agentd.py
@@ -20,6 +20,8 @@ import logging
 import signal
 from contextlib import contextmanager
 from xivo.chain_map import ChainMap
+from xivo.config_helper import parse_config_dir
+from xivo.config_helper import parse_config_file
 from xivo.daemonize import pidfile_context
 from xivo.xivo_logging import setup_logging
 from xivo_agent import ami
@@ -63,6 +65,8 @@ _DEFAULT_CONFIG = {
     'foreground': False,
     'logfile': '/var/log/xivo-agentd.log',
     'pidfile': '/var/run/xivo-agentd.pid',
+    'config_file': '/etc/xivo-agentd/config.yml',
+    'extra_config_files': '/etc/xivo-agentd/conf.d/',
     'ami': {
         'host': 'localhost',
         'username': 'xivo_agent',
@@ -75,8 +79,9 @@ logger = logging.getLogger(__name__)
 
 def main():
     cli_config = _parse_args()
+    file_config = _load_config_file_hierarchy(ChainMap(cli_config, _DEFAULT_CONFIG))
 
-    config = ChainMap(cli_config, _DEFAULT_CONFIG)
+    config = ChainMap(cli_config, file_config, _DEFAULT_CONFIG)
 
     setup_logging(config['logfile'], config['foreground'], config['debug'])
 
@@ -88,6 +93,17 @@ def main():
             logger.exception('Unexpected error:')
         finally:
             logger.info('Stopping xivo-agentd')
+
+
+def _load_config_file_hierarchy(config):
+    main_config_filename = config['config_file']
+    main_config = parse_config_file(main_config_filename)
+    extra_config_file_directory_name = ChainMap(main_config, config)['extra_config_files']
+    extra_configs = parse_config_dir(extra_config_file_directory_name)
+
+    extra_configs.append(main_config)
+
+    return ChainMap(*extra_configs)
 
 
 def _parse_args():
