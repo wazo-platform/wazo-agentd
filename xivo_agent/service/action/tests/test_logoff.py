9afffabb98738895afa528b9c5d1914a3b09fb0d
Etienne Lessard <elessard@proformatique.com>
2016-05-20 08:49:26 -0400

    login_at is now stored in UTC

--- xivo_agent/service/action/tests/test_logoff.py
+++ xivo_agent/service/action/tests/test_logoff.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2013-2015 Avencall
+# Copyright (C) 2013-2016 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -46,7 +46,7 @@ class TestLogoffAction(unittest.TestCase):
         agent_status = Mock()
         agent_status.agent_id = agent_id
         agent_status.agent_number = agent_number
-        agent_status.login_at = datetime.datetime.now()
+        agent_status.login_at = datetime.datetime.utcnow()
         agent_status.queues = [queue]
 
         self.logoff_action.logoff_agent(agent_status)
@@ -56,7 +56,7 @@ class TestLogoffAction(unittest.TestCase):
         self.queue_log_manager.on_agent_logged_off.assert_called_once_with(agent_number, agent_status.extension,
                                                                            agent_status.context, ANY)
         self.agent_status_dao.remove_agent_from_all_queues.assert_called_once_with(agent_id)
-        self.agent_status_dao.log_off_agent(agent_id)
+        self.agent_status_dao.log_off_agent.assert_called_once_with(agent_id)
         self.bus_publisher.publish.assert_called_once_with(
             AgentStatusUpdateEvent(10, AgentStatusUpdateEvent.STATUS_LOGGED_OUT),
         )
