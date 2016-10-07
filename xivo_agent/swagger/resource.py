4593f9499675790a211a641dd665072ba9e420e0
Francois Blackburn <fblackburn@proformatique.com>
2016-10-07 07:09:22 -0400

    convert swagger json to yaml

--- xivo_agent/swagger/resource.py
+++ xivo_agent/swagger/resource.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 Proformatique Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 from flask import make_response
 from flask.ext.restful import Resource
 from pkg_resources import resource_string
@@ -6,8 +23,8 @@ from pkg_resources import resource_string
 class SwaggerResource(Resource):
 
     api_package = "xivo_agent.swagger"
-    api_filename = "api.json"
-    api_path = "/api/api.json"
+    api_filename = "api.yml"
+    api_path = "/api/api.yml"
 
     @classmethod
     def add_resource(cls, api):
@@ -18,4 +35,4 @@ class SwaggerResource(Resource):
             api_spec = resource_string(self.api_package, self.api_filename)
         except IOError:
             return {'error': "API spec does not exist"}, 404
-        return make_response(api_spec, 200, {'Content-Type': 'application/json'})
+        return make_response(api_spec, 200, {'Content-Type': 'application/x-yaml'})
