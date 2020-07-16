686a34ebcec668ce2163c8c3f4b67b56f83ce817
Francois Blackburn <fblackburn@wazo.community>
2020-07-16 10:05:40 -0400

    remove custom ami client/config

--- wazo_agentd/ami/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2012-2019 The Wazo Authors  (see the AUTHORS file)
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-from wazo_agentd.ami.facade import FacadeAMIClient as new_client  # noqa: F401
