7bb98d23b7e21d6b8e5b33fce3098fbee41e6425
Sebastien Duthil <sduthil@wazo.community>
2020-07-23 15:31:42 -0400

    tests: test login/logoff by id

--- integration_tests/suite/test_agents.py
+++ integration_tests/suite/test_agents.py
@@ -1,12 +1,13 @@
 # Copyright 2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from hamcrest import assert_that, calling, has_properties
+from hamcrest import assert_that, calling, has_properties, is_
 
 from wazo_agentd_client.error import AgentdClientError, UNAUTHORIZED
 from xivo_test_helpers.hamcrest.raises import raises
 
 from .helpers.base import BaseIntegrationTest, UNKNOWN_UUID
+from .helpers import fixtures
 
 
 class TestAgents(BaseIntegrationTest):
@@ -19,3 +20,19 @@ class TestAgents(BaseIntegrationTest):
             calling(agentd_client.agents.get_agent_statuses),
             raises(AgentdClientError, has_properties(error=UNAUTHORIZED)),
         )
+
+    @fixtures.user_line_extension(exten='1001', context='default')
+    @fixtures.agent()
+    def test_login_logoff(self, user_line_extension, agent):
+
+        self.agentd.agents.login_agent(
+            agent['id'], user_line_extension['exten'], user_line_extension['context'],
+        )
+
+        status = self.agentd.agents.get_agent_status(agent['id'])
+        assert_that(status.logged, is_(True))
+
+        self.agentd.agents.logoff_agent(agent['id'])
+
+        status = self.agentd.agents.get_agent_status(agent['id'])
+        assert_that(status.logged, is_(False))
