3ad7c2353058e1297ad484d63b17dc5b3ee301be
Julien Alie <jalie@wazo.io>
2022-01-18 11:26:48 -0500

    Changed ArbitraryEvent to Pause/Unpause event
    Why:
    
    * Attempts to standardize bus events by removing
    ArbitraryEvent in microservice

--- wazo_agentd/service/manager/on_queue_agent_paused.py
+++ wazo_agentd/service/manager/on_queue_agent_paused.py
@@ -4,7 +4,7 @@
 import logging
 
 from xivo_dao.helpers import db_utils
-from xivo_bus.resources.common.event import ArbitraryEvent
+from xivo_bus.resources.agent.event import PauseAgentEvent, UnpauseAgentEvent
 
 logger = logging.getLogger(__name__)
 
@@ -17,47 +17,17 @@ class OnQueueAgentPausedManager:
 
     def on_queue_agent_paused(self, agent_id, agent_number, reason, queue):
         self._db_update_agent_status(agent_id, True, reason)
-        bus_event = self._new_agent_paused_event(agent_id, agent_number, reason, queue)
-        self._send_bus_status_update(bus_event, agent_id)
+        event = PauseAgentEvent(agent_id, agent_number, queue, reason)
+        self._send_bus_status_update(event, agent_id)
 
     def on_queue_agent_unpaused(self, agent_id, agent_number, reason, queue):
         self._db_update_agent_status(agent_id, False, reason)
-        bus_event = self._new_agent_unpaused_event(
-            agent_id, agent_number, reason, queue
-        )
-        self._send_bus_status_update(bus_event, agent_id)
-
-    def _new_agent_paused_event(self, id_, number, reason, queue):
-        return self._create_bus_event(
-            'agent_paused', 'status.agent.pause', True, id_, number, reason, queue
-        )
-
-    def _new_agent_unpaused_event(self, id_, number, reason, queue):
-        return self._create_bus_event(
-            'agent_unpaused', 'status.agent.unpause', False, id_, number, reason, queue
-        )
+        event = UnpauseAgentEvent(agent_id, agent_number, queue, reason)
+        self._send_bus_status_update(event, agent_id)
 
     def _db_update_agent_status(self, agent_id, is_paused, reason):
         with db_utils.session_scope():
             self._agent_status_dao.update_pause_status(agent_id, is_paused, reason)
-
-    def _create_bus_event(
-        self, name, routing_key, is_paused, id_, number, reason, queue
-    ):
-        body = {
-            'agent_id': id_,
-            'agent_number': number,
-            'queue': queue,
-            'paused': is_paused,
-            'paused_reason': reason,
-        }
-
-        bus_event = ArbitraryEvent(
-            name=name, body=body, required_acl='events.statuses.agents'
-        )
-        bus_event.routing_key = routing_key
-
-        return bus_event
 
     def _send_bus_status_update(self, event, agent_id):
         with db_utils.session_scope():
@@ -68,4 +38,4 @@ class OnQueueAgentPausedManager:
                 'user_uuid:{uuid}'.format(uuid=user.uuid): True for user in users
             }
             headers['agent_id:{id}'.format(id=str(agent_id))] = True
-            self._bus_publisher.publish(event, headers=headers)
+            self._bus_publisher.publish_soon(event, headers=headers)
