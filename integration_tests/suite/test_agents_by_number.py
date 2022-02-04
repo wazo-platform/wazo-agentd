4b75eafb24af285f0d2f6f10ad0fc05d2277f27b
Julien Alie <jalie@wazo.io>
2022-02-04 08:27:16 -0500

    Modified test to check tenant_uuid in message headers

--- integration_tests/suite/test_agents_by_number.py
+++ integration_tests/suite/test_agents_by_number.py
@@ -15,7 +15,7 @@ class TestAgentsByNumber(BaseIntegrationTest):
     @fixtures.user_line_extension(exten='1001', context='default')
     @fixtures.agent(number='1001')
     def test_event_on_login(self, user_line_extension, agent):
-        event_accumulator = self.bus.accumulator('status.agent', with_headers=True)
+        event_accumulator = self.bus.accumulator('status.agent')
 
         self.agentd.agents.login_agent_by_number(
             agent['number'],
@@ -24,7 +24,7 @@ class TestAgentsByNumber(BaseIntegrationTest):
         )
 
         def event_received():
-            events = event_accumulator.accumulate()
+            events = event_accumulator.accumulate(with_headers=True)
             assert_that(
                 events,
                 contains(
