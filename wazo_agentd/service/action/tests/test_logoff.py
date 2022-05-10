e4d2b1bce1363cd02c8c5c92c7749f2ae8c28d32
Pascal Cadotte Michaud <pcm@wazo.io>
2022-05-10 08:55:57 -0400

    logout: handle unknown interface while unpausing the agent

--- wazo_agentd/service/action/tests/test_logoff.py
+++ wazo_agentd/service/action/tests/test_logoff.py
@@ -51,6 +51,64 @@ class TestLogoffAction(unittest.TestCase):
             Mock(uuid='43'),
         ]
         self.agent_dao.agent_with_id.return_value = Mock(tenant_uuid=tenant_uuid)
+        self.pause_manager.unpause_agent.side_effect = AmidProtocolError(
+            Mock(json=Mock(return_value=[{'Message': 'Interface not found'}]))
+        )
+
+        self.logoff_action.logoff_agent(agent_status)
+
+        self.amid_client.action.assert_called_once_with(
+            'QueueRemove', {'Queue': queue.name, 'Interface': agent_status.interface}
+        )
+        assert_that(
+            self.blf_manager.set_user_blf.call_args_list,
+            contains_inanyorder(
+                call(user_id, 'agentstaticlogin', 'NOT_INUSE', '*{}'.format(agent_id)),
+                call(user_id, 'agentstaticlogin', 'NOT_INUSE', agent_number),
+                call(user_id, 'agentstaticlogoff', 'INUSE', '*{}'.format(agent_id)),
+                call(user_id, 'agentstaticlogoff', 'INUSE', agent_number),
+                call(
+                    user_id, 'agentstaticlogtoggle', 'NOT_INUSE', '*{}'.format(agent_id)
+                ),
+                call(user_id, 'agentstaticlogtoggle', 'NOT_INUSE', agent_number),
+            ),
+        )
+        self.pause_manager.unpause_agent.assert_called_once_with(agent_status)
+        self.queue_log_manager.on_agent_logged_off.assert_called_once_with(
+            agent_number, agent_status.extension, agent_status.context, ANY
+        )
+        self.agent_status_dao.remove_agent_from_all_queues.assert_called_once_with(
+            agent_id
+        )
+        self.agent_status_dao.log_off_agent.assert_called_once_with(agent_id)
+        self.bus_publisher.publish.assert_called_once_with(
+            AgentStatusUpdateEvent(10, AgentStatusUpdateEvent.STATUS_LOGGED_OUT),
+            headers={
+                'user_uuid:42': True,
+                'user_uuid:43': True,
+                'agent_id:10': True,
+                'tenant_uuid': tenant_uuid,
+            },
+        )
+
+    def test_logoff_agent_agent_not_paused(self):
+        agent_id = 10
+        user_id = 42
+        agent_number = '10'
+        queue_name = 'q1'
+        queue = Mock()
+        queue.name = queue_name
+        agent_status = Mock(user_ids=[user_id])
+        agent_status.agent_id = agent_id
+        agent_status.agent_number = agent_number
+        agent_status.login_at = datetime.datetime.utcnow()
+        agent_status.queues = [queue]
+        tenant_uuid = '00000000-0000-4000-8000-000000001ebc'
+        self.user_dao.find_all_by_agent_id.return_value = [
+            Mock(uuid='42'),
+            Mock(uuid='43'),
+        ]
+        self.agent_dao.agent_with_id.return_value = Mock(tenant_uuid=tenant_uuid)
 
         self.logoff_action.logoff_agent(agent_status)
 
