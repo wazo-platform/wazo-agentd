509cfad21650978365677b653d80b8fe1e8ff09a
Julien Alie <jalie@wazo.io>
2022-07-07 11:26:45 -0400

    provide name to bus and use refactored events

--- wazo_agentd/service/manager/on_queue_agent_paused.py
+++ wazo_agentd/service/manager/on_queue_agent_paused.py
@@ -3,8 +3,9 @@
 
 import logging
 
+from functools import partial
 from xivo_dao.helpers import db_utils
-from xivo_bus.resources.agent.event import PauseAgentEvent, UnpauseAgentEvent
+from xivo_bus.resources.agent.event import AgentPausedEvent, AgentUnpausedEvent
 
 logger = logging.getLogger(__name__)
 
@@ -18,27 +19,29 @@ class OnQueueAgentPausedManager:
 
     def on_queue_agent_paused(self, agent_id, agent_number, reason, queue):
         self._db_update_agent_status(agent_id, True, reason)
-        event = PauseAgentEvent(agent_id, agent_number, queue, reason)
+        event = partial(AgentPausedEvent, agent_id, agent_number, queue, reason)
         self._send_bus_status_update(event, agent_id)
 
     def on_queue_agent_unpaused(self, agent_id, agent_number, reason, queue):
         self._db_update_agent_status(agent_id, False, reason)
-        event = UnpauseAgentEvent(agent_id, agent_number, queue, reason)
+        event = partial(
+            AgentUnpausedEvent, agent_id, agent_number, queue, reason
+        )
         self._send_bus_status_update(event, agent_id)
 
     def _db_update_agent_status(self, agent_id, is_paused, reason):
         with db_utils.session_scope():
             self._agent_status_dao.update_pause_status(agent_id, is_paused, reason)
 
-    def _send_bus_status_update(self, event, agent_id):
+    def _send_bus_status_update(self, partial_event, agent_id):
         with db_utils.session_scope():
             logger.debug('Looking for users with agent id %s...', agent_id)
+            tenant_uuid = self._agent_dao.agent_with_id(agent_id).tenant_uuid
+
             users = self._user_dao.find_all_by_agent_id(agent_id)
             logger.debug('Found %s users.', len(users))
             headers = {
                 'user_uuid:{uuid}'.format(uuid=user.uuid): True for user in users
             }
-            headers['agent_id:{id}'.format(id=str(agent_id))] = True
-            tenant_uuid = self._agent_dao.agent_with_id(agent_id).tenant_uuid
-            headers['tenant_uuid'] = tenant_uuid
-            self._bus_publisher.publish_soon(event, headers=headers)
+            event = partial_event(tenant_uuid=tenant_uuid)
+            self._bus_publisher.publish(event, headers=headers)
