1963a0fee33cf78996c1f71fc11c61d70158fb75
nissou91 <achohra@wazo.io>
2022-09-01 13:57:01 -0400

    integration tests: add missing tests for agentd status endpoint

--- integration_tests/suite/test_agentd_status.py
+++ integration_tests/suite/test_agentd_status.py
@@ -2,7 +2,16 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_test_helpers import until
-from hamcrest import assert_that, has_entries, has_entry
+from hamcrest import (
+    assert_that,
+    calling,
+    has_entries,
+    has_entry,
+    has_properties,
+    starts_with,
+)
+from wazo_agentd_client.error import AgentdClientError
+from wazo_test_helpers.hamcrest.raises import raises
 
 from .helpers.base import BaseIntegrationTest
 
@@ -29,3 +38,58 @@ class TestAgentdStatus(BaseIntegrationTest):
             )
 
         until.assert_(check_all_ok, tries=10)
+
+
+class TestAgentdRabbitMQStops(BaseIntegrationTest):
+
+    asset = 'base'
+
+    def setUp(self):
+        super().setUp()
+        self.bus = self.make_bus()
+        until.true(self.bus.is_up, timeout=10)
+
+    def test_given_rabbitmq_stops_when_status_then_bus_status_fail(self):
+        self.stop_service('rabbitmq')
+
+        def rabbitmq_is_shut_down():
+            result = self.agentd.status()
+
+            assert_that(
+                result,
+                has_entries(
+                    bus_consumer=has_entry('status', 'fail'),
+                ),
+            )
+
+        until.assert_(rabbitmq_is_shut_down, timeout=5)
+
+
+class TestAgentdAuthClientStops(BaseIntegrationTest):
+
+    asset = 'base'
+
+    def setUp(self):
+        super().setUp()
+        self.bus = self.make_bus()
+        until.true(self.bus.is_up, timeout=10)
+
+    def test_given_wazo_auth_client_stops_when_status_then_status_fail(self):
+        self.stop_service('agentd')
+        self.stop_service('auth')
+        self.start_service('agentd')
+        agentd = self.make_agentd()
+
+        def _raises_503():
+            assert_that(
+                calling(agentd.status),
+                raises(AgentdClientError).matching(
+                    has_properties(
+                        error=starts_with(
+                            'Could not connect to authentication server on auth'
+                        ),
+                    )
+                ),
+            )
+
+        until.assert_(_raises_503, tries=10)
