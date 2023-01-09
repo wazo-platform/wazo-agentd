50b7b89e8e1bdf01474b1ef9fdf80ad152bf058d
Julien Alie <jalie@wazo.io>
2023-01-09 08:33:30 -0500

    remove deprecated remaining routing_keys

--- wazo_agentd/bus.py
+++ wazo_agentd/bus.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_bus.consumer import BusConsumer as Consumer
@@ -28,4 +28,3 @@ class BusPublisher(Publisher):
 
 class QueueMemberPausedEvent(AMIEvent):
     name = 'QueueMemberPause'
-    routing_key = 'ami.{}'.format(name)
