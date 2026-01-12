cf76776148f30d2755f54c980ecc994c3ce55cff
Julien Alie <jalie@wazo.io>
2026-01-12 15:18:21 -0500

    managers: execute add/remove action even when not logged

--- wazo_agentd/service/manager/on_queue_member_associated.py
+++ wazo_agentd/service/manager/on_queue_member_associated.py
@@ -1,4 +1,4 @@
-# Copyright 2025 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2025-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import annotations
@@ -6,10 +6,7 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING
 
-from xivo_dao.helpers import db_utils
-
 if TYPE_CHECKING:
-    from xivo_dao import agent_status_dao as AgentStatusDAO
     from xivo_dao.agent_dao import _Agent as Agent
 
     from wazo_agentd.dao import _Queue as Queue
@@ -19,11 +16,8 @@ logger = logging.getLogger(__name__)
 
 
 class OnQueueMemberAssociatedManager:
-    def __init__(
-        self, add_to_queue_action: AddToQueueAction, agent_status_dao: AgentStatusDAO
-    ):
-        self._add_to_queue = add_to_queue_action.add_agent_to_queue
-        self._agent_status_dao = agent_status_dao
+    def __init__(self, add_to_queue_action: AddToQueueAction):
+        self._add_to_queue_action = add_to_queue_action
 
     def on_queue_member_associated(self, agent: Agent, queue: Queue, penalty: int):
         if agent.tenant_uuid != queue.tenant_uuid:
@@ -32,11 +26,7 @@ class OnQueueMemberAssociatedManager:
             )
             return
 
-        with db_utils.session_scope():
-            if not (agent_status := self._agent_status_dao.get_status(agent.id)):
-                return
-
         if queue.penalty != penalty:
             queue = queue._replace(penalty=penalty)
 
-        self._add_to_queue(agent_status, queue)
+        self._add_to_queue_action.add_agent_to_queue(agent, queue)
