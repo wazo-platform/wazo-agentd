4ff26b0b37ea89ba545317e199118607f410b5c5
Julien Alie <jalie@wazo.io>
2026-01-14 14:55:26 -0500

    fix add_member_manager dependencies in tests

--- wazo_agentd/service/manager/tests/test_add_member.py
+++ wazo_agentd/service/manager/tests/test_add_member.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import unittest
@@ -12,25 +12,21 @@ class TestAddMemberManager(unittest.TestCase):
     def setUp(self):
         self.add_to_queue_action = Mock()
         self.amid_client = Mock()
-        self.agent_status_dao = Mock()
         self.queue_member_dao = Mock()
         self.member_manager = AddMemberManager(
             self.add_to_queue_action,
             self.amid_client,
-            self.agent_status_dao,
             self.queue_member_dao,
         )
 
     def test_add_agent_to_queue_same_tenant(self):
         agent = Mock(tenant_uuid='fake-tenant', queues=[])
-        agent_status = Mock()
         queue = Mock(tenant_uuid='fake-tenant')
-        self.agent_status_dao.get_status.return_value = agent_status
 
         self.member_manager.add_agent_to_queue(agent, queue)
 
         self.add_to_queue_action.add_agent_to_queue.assert_called_once_with(
-            agent_status, queue
+            agent, queue
         )
         self.queue_member_dao.add_agent_to_queue.assert_called_once_with(
             agent.id, agent.number, queue.name
@@ -47,9 +43,7 @@ class TestAddMemberManager(unittest.TestCase):
 
     def test_add_agent_to_queue_different_tenant(self):
         agent = Mock(tenant_uuid='fake-tenant-1', queues=[])
-        agent_status = Mock()
         queue = Mock(tenant_uuid='fake-tenant-2')
-        self.agent_status_dao.get_status.return_value = agent_status
 
         self.assertRaises(
             QueueDifferentTenantError,
@@ -64,8 +58,6 @@ class TestAddMemberManager(unittest.TestCase):
     def test_add_agent_to_queue_already_in_queue(self):
         queue = Mock(tenant_uuid='fake-tenant', name='queue1')
         agent = Mock(tenant_uuid='fake-tenant', queues=[queue])
-        agent_status = Mock()
-        self.agent_status_dao.get_status.return_value = agent_status
 
         self.assertRaises(
             AgentAlreadyInQueueError,
